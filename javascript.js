9fcb446c06733b2c5805b497d5c6a75ed2b54f61
Misty Duhart <mystie.smith@gmail.com>
2022-03-16 22:15:30 -0400

    javascript file

--- javascript.js
+++ javascript.js
@@ -1,68 +1,76 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-var pwLength = 128;
+
+var pwLength = 12;
 var randomPw = [];
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var pwNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var pwSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "}", "{", "?"];
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-function pwQuestions() {
+// Write password to the #password input
+function writePassword() {
+    var rightQuestions = pwQuestions(); 
+    if(rightQuestions) {
+        var pw1 = generatePassword();
+        var passwordText = document.querySelector("#password");
+  
+     passwordText.value = pw1;
+
+    }
+  }
+  
+function generatePassword() {
+    var password = "";
+    for (var i =  0; i < pwLength; i++) {
+        var randomText = Math.floor(Math.random() * randomPw.length);
+        password = password + randomPw[randomText];  
+    }
+    return password;
+    
+}
+
+function pwQuestions(){
+    randomPw = [];
+
     pwLength = parseInt(prompt("Please enter desired character length between 8 to 128"));
 
     if (pwLength < 8 || pwLength > 128) {
         alert("REMINDER: Must choose between 8 to 128 characters!");
+        return false;
         prompt("Please enter desired character length between 8 to 128")
     }
 
-    if (confirm("Would you like to include Upper Case letters?")); {
-        randomPw = randomPw.concat(upperCase)
+    if (confirm("Would you like to include Upper Case letters?")) {
+        randomPw = randomPw.concat(upperCase);
     }
 
-    if (confirm("Would you like to include Lower Case letters?")); {
-        randomPw = randomPw.concat(lowerCase)
+    if (confirm("Would you like to include Lower Case letters?")) {
+        randomPw = randomPw.concat(lowerCase);
     }
 
-    if (confirm("Would you like to include Numbers?")); {
-        randomPw = randomPw.concat(pwNumbers)
+    if (confirm("Would you like to include Numbers?")) {
+        randomPw = randomPw.concat(pwNumbers);
     }
 
-    if (confirm("Would you like to include Symbols?")); {
-        randomPw = randomPw.concat(pwSymbols)
+    if (confirm("Would you like to include Symbols?")) {
+        randomPw = randomPw.concat(pwSymbols);
     }
-
+    return true;
 
 }
    
 
-// Write password to the #password input
-function writePassword() {
-  var getPassword = randomPw(); 
 
-  if (getPassword) {
-    var password = generatePassword();
-    var passwordText = document.querySelector("password");
-  
-    passwordText.value = password;
-  }
-  
-}
 
-function generatePassword() {
-    var password = "";
 
-    for (var i = 0; i < pwLength; i++) {
-        var random = Math.floor(Math.random() * randomPw.length);
-        password = password + randomPw[random]
-    }
-
-}
 
 
 
