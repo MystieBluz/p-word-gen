b550017603a78869a564ba11b3babccf69781c5f
Misty Duhart <mystie.smith@gmail.com>
2022-03-17 16:09:48 -0400

    image file

--- javascript.js
+++ javascript.js
@@ -9,8 +9,6 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var pwNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var pwSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "}", "{", "?"];
 
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
