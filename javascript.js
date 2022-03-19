d9b6cb2edd02259aaec30d1a673bc3f08cb14fdc
Misty Duhart <mystie.smith@gmail.com>
2022-03-18 20:21:43 -0400

    javascript file

--- javascript.js
+++ javascript.js
@@ -1,45 +1,17 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate")
 
 var pwLength = 12;
 var randomPw = [];
-
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var pwNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var pwSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "}", "{", "?"];
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Write password to the #password input
-function writePassword() {
-    var rightQuestions = pwQuestions(); 
-    if(rightQuestions) {
-        var pw1 = generatePassword();
-        var passwordText = document.querySelector("#password");
-  
-     passwordText.value = pw1;
-
-    }
-  }
-  
-function generatePassword() {
-    var password = "";
-    for (var i =  0; i < pwLength; i++) {
-        var randomText = Math.floor(Math.random() * randomPw.length);
-        password = password + randomPw[randomText];  
-    }
-    return password;
-    
-}
-
+// Password Questions
 function pwQuestions(){
     randomPw = [];
-
     pwLength = parseInt(prompt("Please enter desired character length between 8 to 128"));
-
+    
+//  Alert if correct character length not entered
     if (pwLength < 8 || pwLength > 128) {
         alert("REMINDER: Must choose between 8 to 128 characters!");
         return false;
@@ -64,6 +36,39 @@ function pwQuestions(){
     return true;
 
 }
+
+
+// Write password to the #password input
+function writePassword() {
+    var rightQuestions = pwQuestions(); 
+    if(rightQuestions) {
+        var pw1 = generatePassword();
+        var passwordText = document.querySelector("#password");
+  
+     passwordText.value = pw1;
+
+    }
+  }
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate")
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+  
+function generatePassword() {
+    var password = "";
+    for (var i =  0; i < pwLength; i++) {
+        var randomText = Math.floor(Math.random() * randomPw.length);
+        password = password + randomPw[randomText];  
+    }
+    return password;
+    
+}
+
    
 
 
